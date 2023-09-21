513851b3490da4eff3693a2aaac4f1ed7d1b37c4
catandladybug <98188215+catandladybug@users.noreply.github.com>
2023-09-21 16:08:04 -0700

    upload

---
+++ Object Pool/Assets/Scripts/ClientObjectPool.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Chapter.ObjectPool 
+{ 
+    public class ClientObjectPool : MonoBehaviour 
+    { 
+        private DroneObjectPool _pool; 
+        
+        void Start() 
+        { 
+            _pool = gameObject.AddComponent<DroneObjectPool>();
+        }
+        void OnGUI() 
+        { 
+            if (GUILayout.Button("Spawn Drones")) 
+                _pool.Spawn(); 
+        }
+    }
+}
